b660cf5a24d7ab2e4cf166a95fb641853d181ded
Michele Mei <michele.mei@atscom.it>
2018-04-19 23:40:36 +0200

    catch2 + first dummy test added

---
+++ src/test.cpp
@@ -0,0 +1,13 @@
+#include "../csv.hpp"
+
+#define CATCH_CONFIG_MAIN
+#include "catch.hpp"
+
+// [ _getline_csv_impl ]
+TEST_CASE("_getline_csv_impl: empty string", "[_getline_csv_impl]") {
+	// input           : []
+	// expected str    : []
+	// expected return : true
+	// previous and next tellg pointer must be the same
+	
+}
